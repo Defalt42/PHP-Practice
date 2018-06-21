5e8f37803e20a1dc9215c76db8c87ad84351b0e4
bgilbert33 <bgilbert33@gatech.edu>
2018-06-20 20:02:53 -0500

    Create PHP file and update index.html

---
+++ save-data.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Data saved!</h1>
+    <p>First Name: <?php echo $_POST['first_name'] ?></p>
+  </body>
+</html>
+
+<?php
+  echo "Hello, World!";
+?>
