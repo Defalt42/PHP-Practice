53cc1a14b686f80da8687f0799a29d3f540a27de
bgilbert33 <bgilbert33@gatech.edu>
2018-06-28 14:21:11 -0500

    Add more fields to save-data.php

--- save-data.php
+++ save-data.php
@@ -7,9 +7,8 @@
   <body>
     <h1>Data saved!</h1>
     <p>First Name: <?php echo $_POST['first_name'] ?></p>
+    <p>Last Name: <?php echo $_POST['last_name'] ?></p>
+    <p>Email: <?php echo $_POST['email'] ?></p>
+    <p>Phone: <?php echo $_POST['phone'] ?></p>
   </body>
 </html>
-
-<?php
-  echo "Hello, World!";
-?>
