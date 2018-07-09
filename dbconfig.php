1c04ee2bac81c6a6bb91c56fb2cd0b8831fdaad4
Bryan Gilbert <gilbertb4375@gmail.com>
2018-07-09 15:58:48 -0500

    Change error message

--- dbconfig.php
+++ dbconfig.php
@@ -1,10 +1,10 @@
 <?php
 
-  $conn=mysqli_connect("192.168.1.168","root","mysql","simple_form");
+  $conn=mysqli_connect("localhost","root","mysql","simple_form");
 
   if(!$conn)
   {
-    die("Connection failed: " . mysqli_connect_error());
+    printf("DB connection failed!");
   }
 
 ?>
